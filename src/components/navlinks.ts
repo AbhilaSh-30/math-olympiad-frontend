6ac59d423f5d03da5e23a829ddbd33906140bc65
AbhilaSh-30 <abhilashk2830@gmail.com>
2025-02-17 14:43:01 +0530

    Initial commit

---
+++ src/components/navlinks.ts
@@ -0,0 +1,10 @@
+export const navLinks = [
+  { text: "PSG TECH", link: "https://www.psgtech.edu" },
+  { text: "Home", link: "#" },
+  { text: "Brochure", link: "https://drive.google.com/file/d/1yl0G5YLP-AWkdxj0uPGH4w3GqS4kLegr/view?usp=share_link" },
+  { text: "Syllabus", link: "https://drive.google.com/file/d/1Nl8GSDB-M4zCMqQIiUKQ3nn7jGKmpDAk/view?usp=share_link" },
+  { text: "Instructions", link: "https://drive.google.com/file/d/17vm0Bkd94Wap11Hy_iyad3nmp3Z5NR4E/view?usp=share_link" },
+  { text: "Prize Pool", link: "https://drive.google.com/file/d/1CgrJcjLdOAgx9bM3_ktObkhOgKYSJflL/view?usp=share_link" },
+  { text: "Register here", link: "#Register Here" },
+  { text: "Contact Us", link: "#Co-ordinators" }
+];
